811665f586cbcc347b450706efa212a5c5d40947
shaunbhaskar <73226217+shaunbhaskar@users.noreply.github.com>
2021-11-07 20:59:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+function preload(){
+clown_nose=loadImage('https://i.postimg.cc/tTGC7NVR/Nose.png');
+moustache=loadImage('https://i.postimg.cc/L5D1t6cW/Moustache.png')
+}
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video= createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function draw(){
+image(video,0,0,300,300);
+fill(255,0,0);
+stroke(255,0,0);
+circle(noseX,noseY,);
+image(clown_nose,noseX,noseY,30,30);
+image(moustache,noseX,noseY,100,45);
+}
+function take_snapshot(){
+save('Your_Pic.png');
+}
+
+function modelLoaded(){
+console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results){
+if(results.length > 0){
+console.log(results);
+noseX= results[0].pose.nose.x;
+noseY= results[0].pose.nose.y;
+console.log("nose x=" + noseX);
+console.log("nose y=" + noseY);
+}}
